ffecbb5992eaa76f9e3a90ab9b154acdac18ed65
Sib <132373870+siblinghood@users.noreply.github.com>
2025-03-14 19:16:42 -0500

    Standardized formatting

--- sibsort.js
+++ sibsort.js
@@ -1,21 +1,21 @@
 function sibsort(array) {
     let gap = array.length;
     while (gap > 1) {
-        gap = Math.floor(gap/(4/3))
+        gap = Math.floor(gap / (4 / 3))
         if (gap % 2 == 0) {
             gap--
         }
         if (gap <= 1) {
             gap = 1;
         }
-        for (let index = 0; index < array.length-gap; index += 2) {
+        for (let index = 0; index < array.length - gap; index += 2) {
             if (array[index] > array[index + gap]) {
-                [array[index], array[index+gap]] = [array[index+gap], array[index]];
+                [array[index], array[index+gap]] = [array[index + gap], array[index]];
             }
         }
         for (let index = 1; index < array.length; index += 2) {
             if (array[index] > array[index + gap]) {
-                [array[index], array[index+gap]] = [array[index+gap], array[index]];
+                [array[index], array[index + gap]] = [array[index + gap], array[index]];
             }
         }
     }
@@ -27,7 +27,7 @@ function insertion(array) {
         let pointer = index - 1;
         let indexValue = array[index];
         while (indexValue < array[pointer]) {
-            [array[pointer], array[pointer+1]] = [array[pointer+1], array[pointer]];
+            [array[pointer], array[pointer + 1]] = [array[pointer + 1], array[pointer]];
             if (pointer > 0) {
                 pointer--
             }
