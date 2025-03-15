f892831b63c9c5c7262449e04509f1ef3cd98746
Sib <132373870+siblinghood@users.noreply.github.com>
2025-03-15 00:55:24 -0500

    Added sibsort C implementation by DelayRGC
    thanks delay

---
+++ sibsort.c
@@ -0,0 +1,41 @@
+void sibsort(int*, unsigned int);
+void insertion(int*, unsigned int);
+
+void sibsort(int* array, unsigned int length) {
+    unsigned int gap = length;
+    while (gap > 1) {
+        gap = (gap * 3) >> 2;
+        if (!(gap & 1)) {
+            gap--;
+        }
+        for (unsigned int index = 0; index < length - gap; index += 2) {
+            if (array[index] > array[index + gap]) {
+                int tmp = array[index];
+                array[index] = array[index + gap];
+                array[index + gap] = tmp;
+            }
+        }
+        for (unsigned int index = 1; index < length - gap; index += 2) {
+            if (array[index] > array[index + gap]) {
+                int tmp = array[index];
+                array[index] = array[index + gap];
+                array[index + gap] = tmp;
+            }
+        }
+    }
+    insertion(array, length);
+}
+
+
+void insertion(int* array, unsigned int length) {
+    for (unsigned int index = 1; index < length; index++) {
+        int index_value = array[index];
+        unsigned int pointer = index;
+        while (pointer && array[pointer - 1] > index_value) {
+            array[pointer] = array[pointer - 1];
+            pointer--;
+
+        }
+        array[pointer] = index_value;
+    }
+}
