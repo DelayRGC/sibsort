7aa1de5c0a526b1a662142b2fa9e5ce4f22c14d1
Sib <132373870+siblinghood@users.noreply.github.com>
2025-03-15 22:59:32 -0500

    Updated sibsort.js
    1. Changed gap to multiply instead of dividing by a fraction for readability
    
    2. Fixed bug in insertion code that wasted the first loop.

--- sibsort.js
+++ sibsort.js
@@ -1,36 +1,48 @@
-function sibsort(array) {
-    let gap = array.length;
-    while (gap > 1) {
-        gap = Math.floor(gap / (4 / 3))
-        if (gap % 2 == 0) {
-            gap--
-        }
-        if (gap <= 1) {
-            gap = 1;
-        }
-        for (let index = 0; index < array.length - gap; index += 2) {
-            if (array[index] > array[index + gap]) {
-                [array[index], array[index+gap]] = [array[index + gap], array[index]];
-            }
-        }
-        for (let index = 1; index < array.length; index += 2) {
-            if (array[index] > array[index + gap]) {
-                [array[index], array[index + gap]] = [array[index + gap], array[index]];
-            }
-        }
-    }
-    insertion(array);
+function sibsort(array)
+{
+	let gap = array.length;
+	while (gap > 1)
+	{
+		gap = Math.floor(gap * .75)
+		if (gap % 2 == 0)
+		{
+			gap--
+		}
+		if (gap <= 1)
+		{
+			gap = 1;
+		}
+		for (let index = 0; index < array.length - gap; index += 2)
+		{
+			if (array[index] > array[index + gap])
+			{
+				[array[index], array[index + gap]] = [array[index + gap], array[index]];
+			}
+		}
+		for (let index = 1; index < array.length; index += 2)
+		{
+			if (array[index] > array[index + gap])
+			{
+				[array[index], array[index + gap]] = [array[index + gap], array[index]];
+			}
+		}
+	}
+	insertion(array);
 }
 
-function insertion(array) {
-    for (let index = 0; index < array.length; index++) {
-        let pointer = index - 1;
-        let indexValue = array[index];
-        while (indexValue < array[pointer]) {
-            [array[pointer], array[pointer + 1]] = [array[pointer + 1], array[pointer]];
-            if (pointer > 0) {
-                pointer--
-            }
-        }
-    }
+function insertion(array)
+{
+	for (let index = 1; index < array.length; index++)
+	{
+		let pointer = index - 1
+		let indexValue = array[index]
+		while (indexValue < array[pointer])
+		{
+			[array[pointer], array[pointer + 1]] = [array[pointer + 1], array[pointer]]
+			if (pointer > 0)
+			{
+				pointer--
+			}
+		}
+	}
 }
