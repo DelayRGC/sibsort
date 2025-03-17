83bfe8bdb8a5ddb67142c8ffed48c2ab6935d2cb
Sib <132373870+siblinghood@users.noreply.github.com>
2025-03-17 14:31:56 -0500

    Update sibsort.js (formatting)
    Missed a few semicolons

--- sibsort.js
+++ sibsort.js
@@ -6,7 +6,7 @@ function sibsort(array)
 		gap = Math.floor(gap * .75)
 		if (gap % 2 == 0)
 		{
-			gap--
+			gap--;
 		}
 		if (gap <= 1)
 		{
@@ -34,14 +34,14 @@ function insertion(array)
 {
 	for (let index = 1; index < array.length; index++)
 	{
-		let pointer = index - 1
-		let indexValue = array[index]
+		let pointer = index - 1;
+		let indexValue = array[index];
 		while (indexValue < array[pointer])
 		{
-			[array[pointer], array[pointer + 1]] = [array[pointer + 1], array[pointer]]
+			[array[pointer], array[pointer + 1]] = [array[pointer + 1], array[pointer]];
 			if (pointer > 0)
 			{
-				pointer--
+				pointer--;
 			}
 		}
 	}
