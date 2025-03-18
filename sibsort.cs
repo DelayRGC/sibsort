138f89ef48b312def756b210b5a3b624d85dc3eb
Sib <132373870+siblinghood@users.noreply.github.com>
2025-03-18 18:03:52 -0500

    Added comments

--- sibsort.cs
+++ sibsort.cs
@@ -1,40 +1,36 @@
 static void SibSort(double[] array)
 {
 	int gap = array.Length;
-	while (gap > 1)
+	while (true)
 	{
-		gap = (int)Math.Floor(gap * .75);
-		if (gap % 2 == 0)
+		gap = (int)Math.Floor(gap * .75); // gap will determine how many indexes apart the comparisons happen
+		if (gap % 2 == 0) // shrink gap every loop
 		{
 			gap--; 
 		}
-		if (gap <= 1)
+		if (gap <= 1) // if gap is 1 or lower, break out of the while loop
 		{
-			gap = 1;
+			break;
 		}
-		for (int index = 0; index < array.Length - gap; index += 2)
+		for (int index = 0; index < array.Length - gap; index += 2) // compare and swap gapped odd-even pairs
 		{
 			if (array[index] > array[index + gap])
 			{
-				double temp = array[index];
-				array[index] = array[index + gap];
-				array[index + gap] = temp;
+				(array[index + gap], array[index]) = (array[index], array[index + gap]);
 			}
 		}
-		for (int index = 1; index < array.Length - gap; index += 2)
+		for (int index = 1; index < array.Length - gap; index += 2) // compare and swap gapped even-odd pairs
 		{
 			if (array[index] > array[index + gap])
 			{
-				double temp = array[index];
-				array[index] = array[index + gap];
-				array[index + gap] = temp;
+				(array[index + gap], array[index]) = (array[index], array[index + gap]);
 			}
 		}
 	}
-	Insertion(array);
+	Insertion(array); // once gap is 1 or lower, call insertion to finish
 }
 
-static void Insertion(double[] array)
+static void Insertion(double[] array) // build a growing sorted list by inserting each piece encountered where it fits in that list
 {
 	for (int index = 1; index < array.Length; index++)
 	{
