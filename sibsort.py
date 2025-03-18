72d75e6b49193e4a677eff954e1235c124d48873
Sib <132373870+siblinghood@users.noreply.github.com>
2025-03-18 18:39:19 -0500

    Update sibsort.py

--- sibsort.py
+++ sibsort.py
@@ -1,24 +1,26 @@
 import math
 
 def sibsort(array):
-    gap = len(array)
-    while gap > 3:
-        gap = math.floor(gap * .75)
-        if gap % 2 == 0:
-            gap = gap - 1
-        for index in range(0, len(array) - gap, 2):
-            if array[index] > array[index + gap]:
-                array[index], array[index + gap] = array[index + gap], array[index]
-        for index in range(1, len(array) - gap, 2):
-            if array[index] > array[index + gap]:
-                array[index], array[index + gap] = array[index + gap], array[index]
-    insertion(array)
+	gap = len(array) # gap will determine how many indexes apart the comparisons happen
+	while True:
+		gap = math.floor(gap * .75)
+		if gap % 2 == 0: # keep gap odd
+			gap = gap - 1
+		if gap <= 1: # if gap is 1 or lower, break out of the while loop
+			break
+		for index in range(0, len(array) - gap, 2): # compare and swap gapped odd-even pairs
+			if array[index] > array[index + gap]:
+				array[index], array[index + gap] = array[index + gap], array[index]
+		for index in range(1, len(array) - gap, 2): # // compare and swap gapped even-odd pairs
+			if array[index] > array[index + gap]:
+				array[index], array[index + gap] = array[index + gap], array[index]
+	insertion(array) # once gap is 1 or lower, call insertion to finish
 
-def insertion(array):
-    for index in range(1, len(array)):
-        pointer = index - 1
-        index_value = array[index]
-        while index_value < array[pointer]:
-            array[pointer], array[pointer + 1] = array[pointer + 1], array[pointer]
-            if pointer > 0:
-                pointer -= 1
+def insertion(array): # build a growing sorted list by inserting each piece encountered where it fits in that list
+	for index in range(1, len(array)):
+		pointer = index - 1
+		index_value = array[index]
+		while index_value < array[pointer]:
+			array[pointer], array[pointer + 1] = array[pointer + 1], array[pointer]
+			if pointer > 0:
+				pointer -= 1
