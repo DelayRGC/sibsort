5bd1e57b2ca187592b72e236534427d91847d396
Sib <132373870+siblinghood@users.noreply.github.com>
2025-03-21 00:43:46 -0500

    Update sibsort.js

--- JavaScript/sibsort.js
+++ JavaScript/sibsort.js
@@ -1,25 +1,21 @@
 function sibsort(array)
-{
-	let gap = array.length; // gap will determine how many indexes apart the comparisons happen
-	while (true)
+{																				
+	let gap = array.length; //										gap will determine how many indexes apart the comparisons happen
+	while (gap > 3)
 	{
-		gap = Math.floor(gap * .75) // shrink gap every loop
-		if (gap % 2 == 0) // keep gap odd
+		gap = Math.floor(gap * .75) //									shrink gap every loop and keep it odd
+		if (gap % 2 == 0)
 		{
 			gap--;
 		}
-		if (gap <= 1) // if gap is 1 or lower, break out of the while loop
-		{
-			break;
-		}
-		for (let index = 0; index < array.length - gap; index += 2) // compare and swap gapped odd-even pairs
+		for (let index = 0; index < array.length - gap; index += 2) //					compare and swap gapped odd-even pairs
 		{
 			if (array[index] > array[index + gap])
 			{
 				[array[index], array[index + gap]] = [array[index + gap], array[index]];
 			}
 		}
-		for (let index = 1; index < array.length - gap; index += 2) // compare and swap gapped even-odd pairs
+		for (let index = 1; index < array.length - gap; index += 2) //					compare and swap gapped even-odd pairs
 		{
 			if (array[index] > array[index + gap])
 			{
@@ -27,10 +23,11 @@ function sibsort(array)
 			}
 		}
 	}
-	insertion(array); // once gap is 1 or lower, call insertion to finish
+	insertion(array); //											call insertion to finish
 }
 
-function insertion(array) // build a growing sorted list by inserting each piece encountered where it fits in that list
+// build a growing sorted list by inserting each piece encountered where it fits in that list
+function insertion(array)															
 {
 	for (let index = 1; index < array.length; index++)
 	{
