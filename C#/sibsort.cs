1470491e89b830d99f8147a55114717f840591ad
Sib <132373870+siblinghood@users.noreply.github.com>
2025-03-25 22:46:54 -0500

    Update sibsort.cs

--- C#/sibsort.cs
+++ C#/sibsort.cs
@@ -1,48 +1,52 @@
-static void SibSort(double[] array)
+public static int[] SibSort(int[] array) //									Change datatype of array for your use case!
 {
-	int gap = array.Length;
-	while (gap > 3)
+	int gap = (int) (array.Length * 0.75);
+	while (gap > 1)
 	{
-		gap = (int)Math.Floor(gap * .75); //								gap will determine how many indexes apart the comparisons happen
-		if (gap % 2 == 0) //										shrink gap every loop
-		{
-			gap--; 
-		}
+
 		for (int index = 0; index < array.Length - gap; index += 2) //					compare and swap gapped odd-even pairs
 		{
 			if (array[index] > array[index + gap])
 			{
-				(array[index + gap], array[index]) = (array[index], array[index + gap]);
+				array = Swap(array, i, i + gap);
 			}
 		}
 		for (int index = 1; index < array.Length - gap; index += 2) //					compare and swap gapped even-odd pairs
 		{
 			if (array[index] > array[index + gap])
 			{
-				(array[index + gap], array[index]) = (array[index], array[index + gap]);
+				array = Swap(array, i, i + gap);
 			}
 		}
+		gap = (int) (gap * .75); //									gap will determine how many indexes apart the comparisons happen
+		if (gap % 2 == 0) //										shrink gap every loop
+		{
+			gap--; 
+		}
 	}
-	Insertion(array); //											once gap is 1 or lower, call insertion to finish
+	array = Insertion(array);
+	return array; //											once gap is 1 or lower, call insertion to finish
 }
 
 // build a growing sorted list by inserting each piece encountered where it fits in that list
-static void Insertion(double[] array)
+public static int[] Insertion(int[] array) //									Change datatype of array for your use case!
 {
-	for (int index = 1; index < array.Length; index++)
+	for (int i = 1; i < array.Length; i++)
 	{
-		int pointer = index - 1;
-		double indexValue = array[index];
-		while (indexValue < array[pointer])
+		int iValue = array[i];
+		for (int j = i - 1; j >= 0 && array[j] > iValue; j--)
 		{
-			double temp = array[pointer];
-			array[pointer] = array[pointer + 1];
-			array[pointer + 1] = temp;
-			if (pointer > 0)
-			{
-				pointer--;
-			}
+			array = Swap(array, j, j + 1);
 		}
 	}
+	return array;
 }
-//By RapskaL23
+
+public static int[] Swap(int[] array, int a, int b)
+{
+	int temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+	return array;
+}
+//By RapskaL23 & siblinghood
