538b1560003fec222927cdd853efa255652822ba
Sib <132373870+siblinghood@users.noreply.github.com>
2025-03-25 22:23:28 -0500

    Update sibsort.py

--- Python/sibsort.py
+++ Python/sibsort.py
@@ -1,17 +1,18 @@
 import math
 
 def sibsort(array):
-	gap = len(array) #													gap will determine how many indexes apart the comparisons happen
-	while gap > 3:
-		gap = math.floor(gap * .75)
-		if gap % 2 == 0: #												keep gap odd
-			gap = gap - 1
+	gap = math.floor(len(array) * .75) #											gap will determine how many indexes apart the comparisons happen
+	while gap > 1:
+
 		for index in range(0, len(array) - gap, 2): #									compare and swap gapped odd-even pairs
 			if array[index] > array[index + gap]:
 				array[index], array[index + gap] = array[index + gap], array[index]
 		for index in range(1, len(array) - gap, 2): # 									compare and swap gapped even-odd pairs
 			if array[index] > array[index + gap]:
 				array[index], array[index + gap] = array[index + gap], array[index]
+		gap = math.floor(gap * .75)
+		if gap % 2 == 0: #												keep gap odd
+			gap = gap - 1
 	insertion(array) #													once gap is 1 or lower, call insertion to finish
 
 # build a growing sorted list by inserting each piece encountered where it fits in that list
